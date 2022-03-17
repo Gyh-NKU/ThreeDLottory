28204863a462b487b74e170974d20733bb05670b
guoyunhe <guoyunhe1@outlook.com>
2022-03-17 10:43:33 +0800

    add the random

--- src/com/threedlottery/Main.java
+++ src/com/threedlottery/Main.java
@@ -1,19 +1,29 @@
 package com.threedlottery;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String winNum = "588";
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
+        while (true) {
+            System.out.println("请输入你的投注方式：");
             String mode = sc.next(), inp;
             if (!mode.equals("tractor")) {
+                System.out.println("请输入你的投注号码：");
                 inp = sc.next();
             } else {
                 inp = "";
             }
+            String winNum;
+            Random rd = new Random();
+            Integer[] win = new Integer[3];
+            for (int i = 0; i < 3; i++) {
+                win[i] = rd.nextInt(10);
+            }
+            System.out.println("中奖号码为：" + win[0] + " " + win[1] + " " + win[2]);
 //            System.out.println(mode+" "+inp);
+            winNum = "" + win[0] + win[1] + win[2];
             Lottery3D lottery3D;
             switch (mode) {
                 case "single":
@@ -44,6 +54,7 @@ public class Main {
                     lottery3D = null;
             }
             assert lottery3D != null;
+            System.out.println("你的中奖金额为：");
             System.out.println(lottery3D.getWins());
         }
 
